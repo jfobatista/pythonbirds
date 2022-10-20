e4a464c7b07fa00c75b46970e4a3e1be6e8e0da7
jfobatista <42Glsjhsrtljoao!>
2022-10-20 10:45:48 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,24 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35): #criando parâmetro nome
+    def __init__(self, *filhos, nome=None, idade=35): #criando parâmetro nome
         self.nome = nome #atributo é o self.nome, parâmetro é apenas o nome ou variável dentro do escopo
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luiz')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
+    luiz = Pessoa(nome='Luiz')
+    joao = Pessoa(luiz, nome='Joao')
+    print(Pessoa.cumprimentar(luiz))
+    print(id(luiz))
     try:
-        print(p.cumprimentar(2))  # objeto é passado implicitamente por conta do self
+        print(luiz.cumprimentar())  # objeto é passado implicitamente por conta do self
     except TypeError:
         print('\033[31mVocê passou mais parâmetros do que o necessário.\033[m')
     else:
         print('Deu certo!')
-
-    print(p.nome)
-    p.nome = 'João'
-    print(p.nome)
-    print(p.idade)
+    print(joao.filhos)
+    for filho in joao.filhos:
+        print(filho.nome)
