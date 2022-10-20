2cab80da15552343e319c9c40649f47b6aaa326e
jfobatista <42Glsjhsrtljoao!>
2022-10-20 10:17:42 -0300

    Criada o método cumprimentar de Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa():
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    try:
+        print(p.cumprimentar(2)) #objeto é passado implicitamente por conta do self
+    except TypeError:
+        print('\033[31mVocê passou mais parâmetros do que o necessário.\033[m')
+    else:
+        print('Deu certo!')
